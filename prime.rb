90e66143931c4e030b95c23367a24dd636f88c3b
Heath Mast <hrmast@gmail.com>
2020-04-26 23:57:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 require "pry"
 def prime?(num)
-  (2..(num + 1)).each do |-x|
-    return false if num % x == 0
+  (2..(num + 1)).each do |x|
+    return false if num % -x == 0
     binding.pry
   end
     true
