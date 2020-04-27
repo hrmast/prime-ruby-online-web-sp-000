b52b03ec817ec3b5b447719f51bd72c2d3788938
Heath Mast <hrmast@gmail.com>
2020-04-27 00:07:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,9 +4,7 @@ def prime?(num)
   return false if !num.integer?
   return false if num < 2
   return true if num == 2
-  (2..num - 1)).each do |x|
-    return false if num % x == -0
-    binding.pry
-  end
+  (2..num - 1)).each {|x|
+    return false if num % x == 0}
     true
 end
